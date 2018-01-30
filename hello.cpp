87e38c220a21cf5aeb4921b962b1bc4915021143
awest19 <awest19@mail.csuchico.edu>
2018-01-29 17:17:29 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,7 @@
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Andrew" << std::endl;
     std::cin >> input;
-    std::cout << "Hello, " << input << std::endl;
     return 0;
 }
